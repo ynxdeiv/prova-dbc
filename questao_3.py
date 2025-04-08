73bf7edabd90efcaf0b9633a7da737d11481b503
deiv <deivid652015@gmail.com>
2025-04-08 13:01:11 -0300

    entra 6 valores onde os 3 primeiros representa o salario e os 3 ultimos os gastos respectivamente, o objetivo é subtrair e ver quem economizou mais.

--- questao_3.py
+++ questao_3.py
@@ -1,10 +1,17 @@
+## entra 6 valores onde os 3 primeiros representa o salario e os 3 ultimos os gastos respectivamente, o objetivo é subtrair e ver quem economizou mais.
+
 input_from_question = '300,250,150,50,40,30'
 
 new_list= input_from_question.split(",")
 
-print(new_list)
-
-
+max_economia = 0 
 
 salario = new_list[:3]
-print(salario)
+gastos = new_list[3:]
+
+for _ in range(3):
+
+    economia = int(salario[_])- int(gastos[_])
+
+    if economia >max_economia:
+        max_economia = economia
